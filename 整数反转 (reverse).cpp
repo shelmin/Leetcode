62ce45cdc5f9be353f77f92c60fe59f49f735f91
shelmin <904212011@qq.com>
2018-12-16 23:12:52 +0800

    Create 整数反转 (reverse).cpp

---
+++ 整数反转 (reverse).cpp
@@ -0,0 +1,14 @@
+//该题较简单
+class Solution {
+public:
+    int reverse(int x) {
+         int res = 0;
+         while(x){
+             int temp = res * 10 + x % 10;
+             if(temp / 10 != res) return 0;
+             res = temp;
+             x /= 10;
+         }
+         return res;
+    }
+};
